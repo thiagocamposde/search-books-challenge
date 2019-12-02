d25c7747fdb10db80dac9f3a1743871f5d5fc64e
Thiago de Campos <thiago.campos@dev.getmore.com.br>
2019-12-02 10:10:17 -0300

    Fix api url request

--- client/src/util/request.js
+++ client/src/util/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000'
+// const url = 'http://localhost:5000'
+const url = 'https://search-books-challenge.herokuapp.com/'
 
 const request = axios.create({
   baseURL: `${url}`,
